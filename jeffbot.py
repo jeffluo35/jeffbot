867f66be24a425bdbe9be8f6c4f65e3b896c8e09
Jeff Luo <jeffluo35@gmail.com>
2015-12-02 00:00:33 +0100

    Display data being sent to server

--- jeffbot.py
+++ jeffbot.py
@@ -6,20 +6,29 @@ import socket
 import thread
 import time
 
-ircserver = "irc.choopa.net"
-ircchannel = "#nothing"
+ircserver = "irc.freenode.net"
+ircchannel = "##powder-bots"
 nick = "Jeffbot"
 user = "Jeff"
 
+global ircsock
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def send(data):
+	ircsock.send(data)
+	data = data.strip('\n')
+	print(data)
+
 # Join a channel
 def join(chan):
-	ircsock.send("JOIN "+ chan +"\n")
+	send("JOIN "+ chan +"\n")
 
 # Respond to pings
 def pong():
 	if (data.find("PING") != -1) and (data.find("PRIVMSG") == -1): 
 		source = data.split(":")
-		ircsock.send("PONG :"+ source[1] +"\n")
+		send("PONG :"+ source[1] +"\n")
+		print("PONG :"+ source[1] +"\n")
 # Recieve data from server
 def read(bytes):
 	rawdata = ircsock.recv(bytes)
@@ -27,10 +36,10 @@ def read(bytes):
 	global data
 	data = rawdata.strip('\n\r')
 
-ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ircsock.connect((ircserver, 6667))
-ircsock.send("USER "+ nick +" 0 * :"+ user +"\n")
-ircsock.send("NICK "+ nick +"\n")
+print("Connected to server")
+send("USER "+ nick +" 0 * :"+ user +"\n")
+send("NICK "+ nick +"\n")
 time.sleep(3)
 read(4096)
 pong()
