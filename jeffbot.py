c4560e4176e08976f5f058f14f0ce24434b79f22
Jeff Luo <jeffluo35@gmail.com>
2016-05-18 01:45:51 +0200

    Use python3

--- jeffbot.py
+++ jeffbot.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
 # jeffl35's IRC bot
 
 import socket
-import thread
+import threading
 from time import sleep
 import re
 
@@ -13,61 +12,51 @@ nick = "Jeffbot"
 user = "Jeff"
 
 global ircsock
-
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Send data to the server, but also write it to stdout
 def send(data):
-	ircsock.send(data)
+	ircsock.send(bytes(data, 'UTF-8'))
 	data = data.strip('\n')
 	print(data)
 
-# Join a channel
 def join(chan):
 	send("JOIN "+ chan +"\n")
 
-# Respond to pings
 def pong():
 	if (data.find("PING") != -1) and (data.find("PRIVMSG") == -1): 
-		i = datasplit.index("PING ")
-		i = i + 1
+		i = datasplit.index("PING ") + i
 		send("PONG :"+ datasplit[i] +"\n")
 
-# Recieve data from server
 def read(bytes):
-	rawdata = ircsock.recv(bytes)
+	rawdata = ircsock.recv(bytes).decode('utf-8')
 	print(rawdata)
-	global data
-	global datasplit
+	global data,datasplit
 	data = rawdata.strip('\n\r')
 	datasplit = re.split("[\:\n]", data)
 
 # Initially join a channel
-def initjoin(chan):
-	sleep(3)
-	join(chan)
+class initjoin (threading.Thread):
+	def run(self):
+		sleep(3)
+		join(ircchannel)
 
 def respond(chan, what, response):
 	if (what in datasplit) and (data.find("PRIVMSG") != -1) and (data.find(chan) != -1):
 		send("PRIVMSG "+ chan +" :"+ response +"\n")
-
-def respondraw(chan, what, raw):
-	if (what in datasplit) and (data.find("PRIVMSG") != -1 ) and (data.find(chan) != -1):
-		send(raw +"\n")
 
 def main():
 	ircsock.connect((ircserver, 6667))
 	print("Connected to server")
 	send("USER "+ nick +" 0 * :"+ user +"\n")
 	send("NICK "+ nick +"\n")
-	thread.start_new_thread(initjoin, (ircchannel,))
+	initialjoin = initjoin()
+	initialjoin.start()
 	
-	while 1: # main loop
+	while 1:
 		read(4096)
 		if (data != None):
 			pong()
 			respond(ircchannel, "moo", "moooo")
-			respondraw(ircchannel, ">lol", "KICK "+
 		else:
 			continue
 main()
